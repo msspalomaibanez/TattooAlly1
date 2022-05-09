12709867e924cb81e8a98af1f0c8623267520cd1
AdrianG <aguadixmorales@gmail.com>
2022-05-09 19:03:52 +0200

    Cifrado de contraseña para el login

--- app/src/main/java/com/example/prueba_tattooally/LoginActivity.java
+++ app/src/main/java/com/example/prueba_tattooally/LoginActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 /**
  * Clase en la que se le pedirá al usuario sus datos de inicio de sesión (nickname o correo electrónico) y
@@ -18,7 +22,9 @@ import android.widget.Button;
 public class LoginActivity extends AppCompatActivity {
 
     Button inicio;
-
+    EditText usuario;
+    EditText contrasena;
+    String valor_contrasena;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +32,43 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         inicio = findViewById(R.id.inicio_btn);
+        usuario = findViewById(R.id.usuario_edittxt);
+        contrasena = findViewById(R.id.nick_edittxt);
+        valor_contrasena = String.valueOf(contrasena.getText());
         inicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                valor_contrasena = String.valueOf(contrasena.getText());
                 lanzarInicio(null);
             }
         });
     }
 
     public void lanzarInicio(View view) {
+        System.out.println(convertirContrasena(valor_contrasena));
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     };
+    public String convertirContrasena(String contrasena){
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        byte[] hash = md.digest(contrasena.getBytes());
+        StringBuilder sb = new StringBuilder();
+
+        for(byte b : hash) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+    public boolean comprobarDatos(String usuario, String contrasena){
+        System.out.println();
+        return false;
+    }
 }
